5559ea6c1452f1feef6d5c362d620a8a9cacec9e
James Strutt <10740291+Electrocyte@users.noreply.github.com>
2023-09-26 20:05:37 +0800

    Feature: Add Filename Length Limit Option for FASTA Splitter
    Summary:
    This commit adds a new feature to the FASTA splitter script allowing users to limit the length of the output file names. A new command-line argument (-l or --limit) is introduced which, when used, will limit the filename to the first 10 and the last 20 characters of the header.
    
    Changes:
    - Added a new command-line argument (-l/--limit) to toggle the filename length limitation.
    - Modified the `fasta_splitter` function to check for the `limit_filename_length` parameter and truncate the `clean_header` accordingly.
    - Updated the `argparse` arguments to include the new `-l/--limit` option.

--- fas2fa.py
+++ fas2fa.py
@@ -5,10 +5,10 @@ import re
 import argparse
 from typing import List
 
-def fasta_splitter(input_file: str, output_dir: str) -> None:
+def fasta_splitter(input_file: str, output_dir: str, limit_filename_length: bool = False) -> None:
     with open(input_file, 'r') as f:
         content = f.read().strip()
-        sequences = content.split('>')[1:]  # Split by '>' but skip the first empty item
+        sequences = content.split('>')[1:]
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -16,22 +16,27 @@ def fasta_splitter(input_file: str, output_dir: str) -> None:
     for sequence in sequences:
         lines = sequence.split('\n')
         header = lines[0]
-        # Clean up punctuation, but leave underscores
         clean_header = re.sub(r'[^\w\s_]', '', header).replace(' ', '_')
-        seq = '\n'.join(lines[1:])
 
+        # If filename length should be limited
+        if limit_filename_length:
+            # Take 10 first and 20 last characters
+            clean_header = clean_header[:10] + clean_header[-20:]
+
+        seq = '\n'.join(lines[1:])
         output_file = os.path.join(output_dir, clean_header + '.fasta')
         with open(output_file, 'w') as f:
             f.write('>' + header + '\n' + seq)
 
 def main():
-    parser = argparse.ArgumentParser(description="Splits a FASTA file with multiple headers into individual FASTA files.")
+    parser = argparse.ArgumentParser(description="Splits a FASTA file into individual FASTA files.")
     parser.add_argument("-f", "--input_filepath", help="Path to the input FASTA file.")
-    parser.add_argument("-o", "--output_directory", help="Path to the output directory where individual FASTA files will be saved.")
+    parser.add_argument("-o", "--output_directory", help="Path to the output directory.")
+    parser.add_argument("-l", "--limit", action='store_true', help="Limit the length of the output file names.")
 
     args = parser.parse_args()
 
-    fasta_splitter(args.input_filepath, args.output_directory)
+    fasta_splitter(args.input_filepath, args.output_directory, args.limit)
 
 if __name__ == '__main__':
     main()
