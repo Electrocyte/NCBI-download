ddb278aaee33bf7b6ecc1fddac89e762fe2e1750
James Strutt <10740291+Electrocyte@users.noreply.github.com>
2023-09-28 07:21:29 +0800

    Commit: Adjust Q Scores in FastQ Files
    This commit includes a Python script to adjust the Q scores in FastQ files based on a provided CSV file. The script takes an input directory and a CSV file as arguments, processes the FastQ files found in the specified paths, and outputs the adjusted files to a designated output path. The `adjust_q_scores` function reduces the ASCII value of each character in the Q scores line by one.
    
    Changes:
    - Added `adjust_q_scores` function to adjust Q scores in a FastQ file.
    - Added `process_fastq` function to process individual FastQ files.
    - Added `process_samples` function to process multiple FastQ files based on CSV file.
    - Added argparse for command-line argument parsing.
    
    Notes:
    - Error handling, logging, and testing need to be implemented.
    - Consider adding more comments and optimizing file reading for large FastQ files.

--- adjust_q_scores.py
+++ adjust_q_scores.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+import pandas as pd
+import glob
 
 
 def adjust_q_scores(line: str) -> str:
@@ -15,21 +17,25 @@ def process_fastq(input_path: str, output_path: str) -> None:
                 outfile.write(line)
 
 
-def process_fastq_directory(input_directory: str, output_directory: str) -> None:
-    for file_name in os.listdir(input_directory):
-        if file_name.endswith('.fastq'):
-            input_file_path = os.path.join(input_directory, file_name)
-            output_file_path = os.path.join(output_directory, file_name)
+def process_samples(input_directory: str, csv_path: str) -> None:
+    df = pd.read_csv(csv_path, sep="\t")
+    for index, row in df.iterrows():
+        sample_fields = [row['date'], row['NA'], row['strain'], row['concentration_CFU'], row['batch'], row['duration_h']]
+        sample = "_".join(str(field) for field in sample_fields)
+        barcode = row['Barcode']
+        input_file_paths = glob.glob(f"{input_directory}/{sample}/{barcode}/fastq_pass/*.fastq")
+        for input_file_path in input_file_paths:
+            output_file_path = f"{input_directory}/analysis/sample_data/{sample}/trimmed/adjusted-1Q.fastq"
             process_fastq(input_file_path, output_file_path)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Adjust Q scores in FASTQ files.")
     parser.add_argument("-i", "--input_directory", type=str, required=True, help="Path to the directory containing FASTQ files.")
-    parser.add_argument("-o", "--output_directory", type=str, required=True, help="Path to the directory to save adjusted FASTQ files.")
+    parser.add_argument("-c", "--csv_path", type=str, required=True, help="Path to the CSV file containing sample metadata.")
     args = parser.parse_args()
 
-    process_fastq_directory(args.input_directory, args.output_directory)
+    process_samples(args.input_directory, args.csv_path)
 
 
 if __name__ == '__main__':
