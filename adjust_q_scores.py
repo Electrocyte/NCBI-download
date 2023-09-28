4a4c770f01879eb9c643e4614cb1106bb9702cba
James Strutt <10740291+Electrocyte@users.noreply.github.com>
2023-09-28 10:14:56 +0800

    Implement FASTQ file processing and concatenation Enhanced the script to process FASTQ files by adjusting the Q scores and concatenate them into a single file. The script reads metadata from a CSV file and processes each FASTQ file according to the metadata. The Q scores are adjusted, and the results are stored in temporary files which are then concatenated into a final output file. Temporary files are removed after concatenation.

--- adjust_q_scores.py
+++ adjust_q_scores.py
@@ -2,6 +2,16 @@ import argparse
 import os
 import pandas as pd
 import glob
+from itertools import zip_longest
+import shutil
+
+
+# saw this in the python docs. looks like exactly what you need
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into non-overlapping fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
 
 
 def adjust_q_scores(line: str) -> str:
@@ -12,12 +22,12 @@ def process_fastq(input_path: str, output_path: str) -> None:
     output_dir = os.path.dirname(output_path)  # Get the directory part of output_path
     os.makedirs(output_dir, exist_ok=True)  # Create the directory if it does not exist
 
-    with open(input_path, 'r') as infile, open(output_path, 'a') as outfile:
-        for index, line in enumerate(infile):
-            if (index + 1) % 4 == 0:
-                outfile.write(adjust_q_scores(line))
-            else:
-                outfile.write(line)
+    with open(input_path, 'r') as infile, open(output_path, 'w') as outfile:
+        for readID, sequence, plus, quality in grouper(infile, 4, None):
+            outfile.write(readID)
+            outfile.write(sequence)
+            outfile.write(plus)
+            outfile.write(adjust_q_scores(quality))
 
 
 def process_samples(input_directory: str, csv_path: str) -> None:
@@ -28,16 +38,27 @@ def process_samples(input_directory: str, csv_path: str) -> None:
         barcode = row['Barcode']
         input_file_paths = glob.glob(f"{input_directory}/{sample}/{barcode}/fastq_pass/*.fastq")
         print(f"Processing {len(input_file_paths)} FASTQ files for sample {sample}...")
-        for input_file_path in input_file_paths:
-            os.makedirs(f"{input_directory}/analysis/", exist_ok=True)
-            os.makedirs(f"{input_directory}/analysis/sample_data/", exist_ok=True)
-            os.makedirs(f"{input_directory}/analysis/sample_data/{sample}/", exist_ok=True)
-            os.makedirs(f"{input_directory}/analysis/sample_data/{sample}/trimmed/", exist_ok=True)
-            output_file_path = f"{input_directory}/analysis/sample_data/{sample}/trimmed/{barcode}_adjusted-1Q.fastq"
-            # If it's the first file, ensure the output file does not exist
-            if input_file_path == input_file_paths[0] and os.path.exists(output_file_path):
-                os.remove(output_file_path)
-            process_fastq(input_file_path, output_file_path)
+
+        # Store the temporary output file paths
+        temp_output_file_paths = []
+        for n, input_file_path in enumerate(input_file_paths):
+            os.makedirs(f"{input_directory}/{sample}/adjusted/", exist_ok=True)
+            temp_output_file_path = f"{input_directory}/{sample}/adjusted/{n}-{barcode}_adjusted-1Q.fastq"
+            process_fastq(input_file_path, temp_output_file_path)
+            temp_output_file_paths.append(temp_output_file_path)
+
+        os.makedirs(f"{input_directory}/analysis/sample_data/{sample}/trimmed/", exist_ok=True)
+        output_file_path = f"{input_directory}/analysis/sample_data/{sample}/trimmed/{barcode}_adjusted-1Q.fastq"
+
+        # Concatenate all temporary output files to the final output file path
+        with open(output_file_path, 'wb') as outfile:
+            for file_path in temp_output_file_paths:
+                with open(file_path, 'rb') as infile:
+                    shutil.copyfileobj(infile, outfile)
+
+        # Optionally, you can remove the temporary files after concatenation.
+        for file_path in temp_output_file_paths:
+            os.remove(file_path)
 
 
 def main() -> None:
