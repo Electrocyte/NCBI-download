aa18cb664a702b5ced0f517c986ac1c7fee59d9f
James Strutt <10740291+Electrocyte@users.noreply.github.com>
2023-09-28 09:55:44 +0800

    Change output file mode to append in process_fastq

--- adjust_q_scores.py
+++ adjust_q_scores.py
@@ -12,7 +12,7 @@ def process_fastq(input_path: str, output_path: str) -> None:
     output_dir = os.path.dirname(output_path)  # Get the directory part of output_path
     os.makedirs(output_dir, exist_ok=True)  # Create the directory if it does not exist
 
-    with open(input_path, 'r') as infile, open(output_path, 'w') as outfile:
+    with open(input_path, 'r') as infile, open(output_path, 'a') as outfile:
         for index, line in enumerate(infile):
             if (index + 1) % 4 == 0:
                 outfile.write(adjust_q_scores(line))
@@ -34,6 +34,9 @@ def process_samples(input_directory: str, csv_path: str) -> None:
             os.makedirs(f"{input_directory}/analysis/sample_data/{sample}/", exist_ok=True)
             os.makedirs(f"{input_directory}/analysis/sample_data/{sample}/trimmed/", exist_ok=True)
             output_file_path = f"{input_directory}/analysis/sample_data/{sample}/trimmed/{barcode}_adjusted-1Q.fastq"
+            # If it's the first file, ensure the output file does not exist
+            if input_file_path == input_file_paths[0] and os.path.exists(output_file_path):
+                os.remove(output_file_path)
             process_fastq(input_file_path, output_file_path)
 
 
